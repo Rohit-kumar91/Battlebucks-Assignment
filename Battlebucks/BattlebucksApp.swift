3c3829597aea855ce67d11161f0238d9424f41b5
Rohit Kumar <rohitku.prajapati@gmail.com>
2024-09-29 11:06:06 +0530

    Initial Setup.

---
+++ Battlebucks/BattlebucksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BattlebucksApp.swift
+//  Battlebucks
+//
+//  Created by Rohit Kumar 2 on 29/09/24.
+//
+
+import SwiftUI
+
+@main
+struct BattlebucksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BattlebucksTests/BattlebucksTests.swift
@@ -0,0 +1,17 @@
+//
+//  BattlebucksTests.swift
+//  BattlebucksTests
+//
+//  Created by Rohit Kumar 2 on 29/09/24.
+//
+
+import Testing
+@testable import Battlebucks
+
+struct BattlebucksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
